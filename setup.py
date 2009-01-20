67916350983309957de314d963ebad4627fcbb1c
Christophe Pradal <christophe.pradal@inria.fr>
2009-01-20 08:44:05 +0000

    Update the version number
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk/WeberPenn@5967 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ namespace = 'vplants'
 if __name__ == '__main__':
     
     setup(name='vplants.weberpenn',
-          version='0.6.0',
+          version='0.6.1',
           author='C. Pradal',
           description='Implementation of the tree model published by Weber and Penn',
           url='',
