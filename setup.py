60515542c1b9312620f5d18d4ee7dfb0840bd1b1
Thomas Cokelaer <cokelaer@gmail.com>
2009-01-23 12:33:12 +0000

    * update version number * changes years * update the make_dist.sh script with the proper packages
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk/WeberPenn@5974 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ namespace = 'vplants'
 if __name__ == '__main__':
     
     setup(name='vplants.weberpenn',
-          version='0.6.1',
+          version='0.6.2',
           author='C. Pradal',
           description='Implementation of the tree model published by Weber and Penn',
           url='',
