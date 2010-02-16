fc51085982ff72aad4a7496f40dc3c1eb3f6399a
Christophe Pradal <christophe.pradal@inria.fr>
2010-02-16 14:15:11 +0000

    Replace author by authors in the setup.py to conform with metainfo.ini
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk/WeberPenn@7985 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ from os.path import join as pj
 
 from openalea.deploy.metainfo import read_metainfo
 metadata = read_metainfo('metainfo.ini', verbose=True)
-
 for key,value in metadata.iteritems():
     exec("%s = '%s'" % (key, value))
 
